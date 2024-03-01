d2ac8c6d9056891f4b1bef2b010dc0bbadcea63a
enaguero <enaguero@gmail.com>
2024-03-01 18:26:09 +0000

    Example for creating end-points

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+    people_query = Person.query.all()
+    all_people = list(map(lambda x: x.serialize(), people_query))
+
+    return jsonify(all_people), 200
+
+@app.route('/people', methods=['POST'])
+def create_people():
+    
+    body = request.get_json()
+    print(body)
+    user1 = Person(name=body['name'], last_name=body['last_name'])
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify({"msg": "The person has been created"}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
